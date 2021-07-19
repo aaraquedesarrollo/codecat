9e5ec7c46de8057882780421d33fe9f1585cf708
Jose Maria Zamora <jzamora@biada.net>
2021-07-19 12:20:40 +0200

    mantenemos como se manejaban anteriror mente los errores

--- servidor/rutas/trabajo.js
+++ servidor/rutas/trabajo.js
@@ -12,9 +12,8 @@ const { validarErrores, authMiddleware } = require("../middlewares");
 const router = express.Router();
 
 router.get("/listado", authMiddleware, async (req, res, next) => {
-  const id = req.idUsuario;
   try {
-    const trabajos = await listarTrabajos(id);
+    const trabajos = await listarTrabajos();
     res.json(trabajos);
   } catch (err) {
     next(err);
