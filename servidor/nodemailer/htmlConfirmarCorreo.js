97d4098e36bd718251cdb3643e1d892e7c6f9b9b
Jose Maria Zamora <jzamora@biada.net>
2021-07-20 13:15:35 +0200

    corregido mensaje del email donde no cogia los estilos aplicados

--- servidor/nodemailer/htmlConfirmarCorreo.js
+++ servidor/nodemailer/htmlConfirmarCorreo.js
@@ -6,76 +6,35 @@ const htmlConfirmarCorreo = (hash) => `<html lang="es-ES">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet"/>
     <title>Document</title>
   </head>
   <body>
-    <style>
-      header {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-bottom: 2px solid #225a8f;
-      }
-      h1 {
-        font-family: "Press Start 2P", cursive;
-        color: #4987c2;
-      }
-      main {
-        margin-top: 25px;
-        margin-bottom: 25px;
-        display: flex;
-        justify-content: space-evenly;
-      }
-      p {
-        font-size: 20px;
-        font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
-      }
-      button {
-        background-color: #4987c2;
-        padding: 15px;
-        border: 0;
-        border-radius: 5px;
-        cursor: pointer;
-      }
-      button a{
-        color: aliceblue;
-        font-size: 15px;
-        text-decoration: none;
-      }
-      aside {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      footer {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-    </style>
-    <header>
-      <h1>CodeCat Productions</h1>
+    <header style="display:flex; justify-content:center; align-items:center; border-bottom:2px solid #225a8f;">
+      <h1 style=" font-family: "Press Start 2P", cursive;color: #4987c2;", cursive">CodeCat Productions</h1>
     </header>
-    <main>
-      <p>
+       
+    <main style="margin-top:25px; margin-bottom:25px; display:flex; justify-content:space-evenly;">
+       
+      <p style="font-size: 20px;font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;">
         Por favor, haz click en el siguiente link para confirmar tu dirección de
         email:
       </p>
-      <button type="button">
-        <a href="${process.env.URL_API}usuarios/confirmar-email/${hash}">Confirmar email</a>
+       
+      <button type="button" style="background-color: #4987c2; padding: 15px;border: 0;border-radius: 5px;cursor: pointer;">
+        
+        <a href="${process.env.URL_API}usuarios/confirmar-email/${hash}" style="color: aliceblue;font-size: 15px;text-decoration: none;">Confirmar email</a>
         </button>
     </main>
-    <aside>
-      <p>
+        
+    <aside style="display: flex;justify-content: center;align-items: center;">
+      <p style="font-size: 20px;font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;">
         ¡Con CodeCat podrás convertirte en un gran programador, disfruta del
         juego!
       </p>
     </aside>
-    <footer>CodeCat &copy;</footer>
+        
+    <footer style="display: flex;justify-content: center;align-items: center;">CodeCat &copy;</footer>
   </body>
 </html>`;
 
