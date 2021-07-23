42429437280fb5d8dada5c795f8f7d90aaf2b2f6
Albert Araque <aaraquedesarrollo@gmail.com>
2021-07-23 12:27:45 +0200

    Ruta para obtener trabajo y validaciones en nombreGato

--- servidor/rutas/codecat.js
+++ servidor/rutas/codecat.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 const {
   obtenerNivelUsuario,
   obtenerSigueinteNivel,
@@ -11,7 +12,7 @@ const {
   obtenerUsuario,
   modificarUsuario,
 } = require("../../bd/controladores/usuarioController");
-const { authMiddleware } = require("../middlewares");
+const { authMiddleware, validarErrores } = require("../middlewares");
 
 const router = express.Router();
 
@@ -37,6 +38,12 @@ router.get("/cargar-informacion", authMiddleware, async (req, res, next) => {
 
 router.put(
   "/nombre-gato/:nombreGato",
+  check("nombreGato")
+    .isLength({ min: 3 })
+    .withMessage("El nombre del gato debe tener al menos 3 caracteres")
+    .matches(/^[a-zA-Z\s]*$/)
+    .withMessage("El nombre solo puede contener letras y espacios"),
+  validarErrores,
   authMiddleware,
   async (req, res, next) => {
     try {
